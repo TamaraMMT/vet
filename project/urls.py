065e6b8a16d5aab70e814f5e403690db42772180
Tamara MT <tamaramolinast@gmail.com>
2023-09-12 06:38:26 -0400

    create authors app

--- project/urls.py
+++ project/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('blog/', include('blog.urls')),
+    path('authors/', include('authors.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
