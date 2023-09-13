e9774570b65b5d51533f01520e8c6c38a3eeaeac
Tamara MT <tamaramolinast@gmail.com>
2023-09-13 19:53:28 -0400

    file improvements

--- authors/forms.py
+++ authors/forms.py
@@ -20,37 +20,41 @@ def strong_password(password):
 
 class RegistrationForm(forms.ModelForm):
     username = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Your username'}),
         label='Username',
         help_text=(
             'Username must have letters, numbers or one of those @.+-_. '
-            'The length should be between 4 and 150 characters.'
+            'The length should be between 4 and 30 characters.'
         ),
         error_messages={
             'required': 'This field must not be empty',
-            'min_length': 'Username must have at least 4 characters',
-            'max_length': 'Username must have less than 150 characters',
+            'min_length': 'Minimum 4 characters',
+            'max_length': 'Max 30 characters',
         },
-        min_length=4, max_length=150,
+        min_length=4, max_length=30,
     )
 
     first_name = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Your first name'}),
         error_messages={'required': 'Write your first name'},
         label='First name'
     )
 
     last_name = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Your last name'}),
         error_messages={'required': 'Write your last name'},
-        label='Last name'
+        label='Last name',
     )
 
     email = forms.EmailField(
+        widget=forms.EmailInput(attrs={'placeholder': 'Your email'}),
         error_messages={'required': 'E-mail is required'},
         label='E-mail',
         help_text='yourname@example.com',
     )
 
     password = forms.CharField(
-        widget=forms.PasswordInput(),
+        widget=forms.PasswordInput(attrs={'placeholder': 'Your password'}),
         error_messages={
             'required': 'Password must not be empty'
         },
@@ -63,7 +67,8 @@ class RegistrationForm(forms.ModelForm):
         label='Password'
     )
     password2 = forms.CharField(
-        widget=forms.PasswordInput(),
+        widget=forms.PasswordInput(
+            attrs={'placeholder': 'Repeat your password'}),
         label='Password2',
         error_messages={
             'required': 'Please, repeat your password'
@@ -74,17 +79,20 @@ class RegistrationForm(forms.ModelForm):
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'password']
 
-        widgets = {
-            'first_name': forms.TextInput(attrs={'placeholder': 'Your first name'}),
-            'last_name': forms.TextInput(attrs={'placeholder': 'Your last name'}),
-            'username': forms.TextInput(attrs={'placeholder': 'Your username'}),
-            'email': forms.EmailInput(attrs={'placeholder': 'Your email'}),
-            'password': forms.PasswordInput(attrs={'placeholder': 'Your password'}),
-        }
+    def clean(self):
+        cleaned_data = super().clean()
 
-    def clean_password2(self):
-        password1 = self.cleaned_data.get('password')
-        password2 = self.cleaned_data.get('password2')
+        password = cleaned_data.get('password')
+        password2 = cleaned_data.get('password2')
 
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Passwords do not match.")
+        if password != password2:
+            password_confirmation_error = ValidationError(
+                'The passwords do not equal',
+                code='invalid'
+            )
+            raise ValidationError({
+                'password': password_confirmation_error,
+                'password2': [
+                    password_confirmation_error,
+                ],
+            })
