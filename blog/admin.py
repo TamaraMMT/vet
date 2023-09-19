8df58847d5b010584a1f381ea36fb31b518dee99
Tamara MT <tamaramolinast@gmail.com>
2023-09-19 12:17:08 -0400

    changes in admin.py and header if authenticated

--- blog/admin.py
+++ blog/admin.py
@@ -1,16 +1,21 @@
 from django.contrib import admin
 from .models import Category, PostBlog
 
+
 class CategoryAdmin(admin.ModelAdmin):
     ...
 
 
 @admin.register(PostBlog)
 class PostAdmin(admin.ModelAdmin):
-    ...
+    list_display = ['id', 'title', 'author', 'created_ad']
+    list_display_links = 'id', 'title', 'author', 'created_ad'
+    search_fields = 'id', 'title', 'description', 'author'
+    list_filter = 'category', 'author', 'created_ad'
+    list_per_page = 10
+    prepopulated = {
+        'slug': ('title',)
+    }
 
 
 admin.site.register(Category, CategoryAdmin)
-
-
-
