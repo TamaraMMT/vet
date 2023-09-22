0186cd45ae37db9928728e7b3ac5e1db87eaeef2
Tamara MT <tamaramolinast@gmail.com>
2023-09-21 21:13:51 -0400

    time zone

---
+++ authors/tests/test_views_create_post_blog.py
@@ -0,0 +1,7 @@
+from django.urls import reverse
+from blog.tests.test_post_base import PostTestBase
+
+
+class CreatePostViewTest(PostTestBase):
+    def test_create_post_status_code(self):
+        ...
