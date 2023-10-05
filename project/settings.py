5d353566038779e3e5f54427b1cc472e872d002d
Tamara MT <tamaramolinast@gmail.com>
2023-10-05 14:01:45 -0300

    allowed_hosts

--- project/settings.py
+++ project/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
-
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 # Application definition
 
