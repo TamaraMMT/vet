847ee027e013def095920764b278447d26c01a38
Tamara MT <tamaramolinast@gmail.com>
2023-10-07 13:25:10 -0300

    Organize Tests

--- blog/tests/test_models_category_post_blog.py
+++ blog/tests/test_models_category_post_blog.py
@@ -2,25 +2,6 @@ from django.forms import ValidationError
 from blog.tests.test_post_base import PostTestBase
 
 
-class PostModelTest(PostTestBase):
-    def setUp(self):
-        self.post = self.make_post()
-        return super().setUp()
-
-    def test_posts_title_raise_error_if_title_has_more_65_chars(self):
-        self.post.title = 'A' * 66
-        with self.assertRaises(ValidationError):
-            self.post.full_clean()
-
-    def test_posts_article_raise_error_if_title_has_more_65_chars(self):
-        self.post.article = 'A' * 1501
-        with self.assertRaises(ValidationError):
-            self.post.full_clean()
-
-    def test_posts_str_representation(self):
-        self.assertEqual(str(self.post), 'My first post')
-
-
 class CategoryModelTest(PostTestBase):
     def setUp(self):
         self.post = self.make_post()
