75c4b1081227be115e46efb731e8361a639038ba
Tamara MT <tamaramolinast@gmail.com>
2023-10-24 16:42:58 -0300

    duplicate api/ url fixed

--- blog/urls.py
+++ blog/urls.py
@@ -12,12 +12,12 @@ urlpatterns = [
         name='category_posts'
     ),
     path(
-        'api/posts/api/v1/',
+        'api/posts/v1/',
         views.BlogAPIViewSet.as_view({'get': 'list'}),
         name='list_post'
     ),
     path(
-        'api/posts/api/v1/<int:pk>/',
+        'api/posts/v1/<int:pk>/',
         views.BlogAPIViewSet.as_view({'get': 'retrieve'}),
         name='detail_post'
     ),
