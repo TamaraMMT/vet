b3120eb6c6e990f08f74a325b77d7d12795000d7
Tamara MT <tamaramolinast@gmail.com>
2023-10-24 16:34:15 -0300

    added token URLs

--- authors/urls.py
+++ authors/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
-from . import views
+from authors import views
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 app_name = 'authors'
 
@@ -23,5 +29,19 @@ urlpatterns = [
         views.ProfileView.as_view(),
         name='profile'
     ),
+    path(
+        'api/token/',
+        TokenObtainPairView.as_view(),
+        name='token_obtain_pair'
+    ),
+    path(
+        'api/token/refresh/',
+        TokenRefreshView.as_view(),
+        name='token_refresh'
+    ),
+    path(
+        'api/token/verify/',
+        TokenVerifyView.as_view(),
+        name='token_verify'),
 
 ]
