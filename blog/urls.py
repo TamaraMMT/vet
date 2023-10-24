8e333782d6f114c72aece60a4bf5695ef0753fe4
Tamara MT <tamaramolinast@gmail.com>
2023-10-24 14:35:31 -0300

    path for API Blog, list and retrieve

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,20 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.BlogListView.as_view(), name='blog'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='posts'),
-    path('category/<int:pk>/', views.CategoryPostsListView.as_view(), name='category_posts'),   # noqa:E501
+    path(
+        'category/<int:pk>/',
+        views.CategoryPostsListView.as_view(),
+        name='category_posts'
+    ),
+    path(
+        'api/posts/api/v1/',
+        views.BlogAPIViewSet.as_view({'get': 'list'}),
+        name='list_post'
+    ),
+    path(
+        'api/posts/api/v1/<int:pk>/',
+        views.BlogAPIViewSet.as_view({'get': 'retrieve'}),
+        name='detail_post'
+    ),
+
 ]
