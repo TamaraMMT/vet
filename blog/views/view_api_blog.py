a1de06ae163a549edf15d0dd5c851c09d83cdd5e
Tamara MT <tamaramolinast@gmail.com>
2023-10-27 21:00:51 -0300

    added category id filter

--- blog/views/view_api_blog.py
+++ blog/views/view_api_blog.py
@@ -1,5 +1,11 @@
 
+from drf_spectacular.utils import (
+    extend_schema_view,
+    extend_schema,
+    OpenApiParameter,
+    OpenApiTypes,
 
+)
 from rest_framework import viewsets
 
 from blog.models import PostBlog
@@ -9,9 +15,30 @@ from blog.serializers import (
 )
 
 
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                'categorys',
+                OpenApiTypes.INT,
+                description='Category IDs to filter',
+            ),
+        ]
+    )
+)
 class BlogAPIViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = PostBlog.objects.all()
     serializer_class = PostBlogSerializer
+    queryset = PostBlog.objects.all()
+
+    def get_queryset(self):
+        category_id = self.request.query_params.get(
+            'categorys')
+        if category_id:
+            queryset = PostBlog.objects.filter(
+                category__id=category_id)
+        else:
+            queryset = super().get_queryset()
+        return queryset.distinct()
 
     def get_serializer_class(self):
         if self.action == "retrieve":
